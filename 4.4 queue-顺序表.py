2db294029248b08fbed43f9bf043061aafcd3835
Fade-Smile <81165176+Fade-Smile@users.noreply.github.com>
2023-12-21 12:29:13 +0000

    Add files via upload

---
+++ 4.4 queue-顺序表.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+'''
+@Project :Python数据结构和算法 
+@File    :4.4 queue-顺序表.py
+@Author  :Sunshine
+@Date    :21/12/2023 11:56 
+'''
+
+# 队列 -- 顺序列表实现
+
+
+class Queue:
+    def __init__(self):
+        # 以列表的最后一个元素作为队尾
+        self.items = []
+
+    # 判断该是否为空
+    def isEmpty(self):
+        return self.items == []
+
+    # 判断长度
+    def length(self):
+        return len(self.items)
+
+    # 队尾添加元素
+    def push(self, item):
+        self.items.append(item)                       # 从尾部添加, 时间复杂的度 O(1)
+        # self.items.insert(0, item)      # 从头部添加, 时间复杂度O(n)
+
+    # 抛出队首
+    def pop(self):
+        if self.isEmpty():
+            raise ValueError('队列是空的')
+        return self.items.pop(0)
+
+    # 获取队首的值
+    def peek(self):
+        if self.isEmpty():
+            raise ValueError('列表为空')
+        return self.items[0]
+
+
+if __name__ == '__main__':
+    queue = Queue()
+    queue.push(1)
+    queue.push(2)
+    queue.push(3)
+    queue.push(4)
+    print(queue.length())
+    print()
+    print(queue.peek())
+    print()
+    print(queue.pop())
+    print(queue.pop())
+    print(queue.pop())
+    print(queue.pop())
+    print(queue.pop())
---
+++ 4.5 queue-LinkedList.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+'''
+@Project :Python数据结构和算法 
+@File    :4.5 queue-LinkedList.py
+@Author  :Sunshine
+@Date    :21/12/2023 12:15 
+'''
+
+
+# 队列 -- 链表实现
+
+class Node:
+    def __init__(self, data, _next=None):
+        self.data = data  # 数字域
+        self.next = _next  # 指针域
+
+
+class Queue:
+    def __init__(self):
+        self.head = None  # 队列的队头
+        self.rear = None  # 队列的队尾
+        self._length = 0  # 队列的长度
+
+    # 判断该是否为空
+    def isEmpty(self):
+        return self._length == 0
+
+    # 判断长度
+    def length(self):
+        return self._length
+
+    # 队尾添加元素
+    def push(self, item):
+        node = Node(item)
+        # 如果队列为空
+        if self.isEmpty():
+            self.head = node
+            self.rear = node
+        else:
+            self.rear.next = node
+            self.rear = node
+        self._length += 1
+
+    # 抛出队首
+    def pop(self):
+        if self.isEmpty():
+            raise ValueError('队列是空的')
+        value = self.head.data
+        self.head = self.head.next
+        self._length -= 1
+        return value
+
+    # 获取队首的值
+    def peek(self):
+        if self.isEmpty():
+            raise ValueError('列表为空')
+        return self.head.data
+
+
+if __name__ == '__main__':
+    queue = Queue()
+    queue.push(1)
+    queue.push(2)
+    queue.push(3)
+    queue.push(4)
+    print(queue.length())
+    print()
+    print(queue.peek())
+    print()
+    print(queue.pop())
+    print(queue.pop())
+    print(queue.pop())
+    print(queue.pop())
+    print(queue.pop())
