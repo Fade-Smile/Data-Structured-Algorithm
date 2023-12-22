73d35c6809f62c031208d679e93e687c49a860f2
Fade-Smile <81165176+Fade-Smile@users.noreply.github.com>
2023-12-22 14:16:18 +0000

    Add files via upload

--- 4.5 queue-LinkedList.py
+++ 4.5 queue-LinkedList.py
@@ -9,33 +9,23 @@
 
 
 # 队列 -- 链表实现
-
 class Node:
     def __init__(self, data, _next=None):
-        self.data = data  # 数字域
-        self.next = _next  # 指针域
-
-
+        self.data = data                # 数字域
+        self.next = _next               # 指针域
 class Queue:
-    def __init__(self, size):
-        self.head = None  # 队列的队头
-        self.rear = None  # 队列的队尾
-        self._length = 0  # 队列的长度
-        self.size = size  # 队列的最大长度
-
-    # 判断该是否为空  时间复杂度O(1)
+    def __init__(self):
+        self.head = None                # 队列的队头
+        self.rear = None                # 队列的队尾
+        self._length = 0                # 队列的长度
+    # 判断该是否为空
     def isEmpty(self):
         return self._length == 0
-
-    # 判断长度    时间复杂度O(1)
+    # 判断长度
     def length(self):
         return self._length
-
-    # 队尾添加元素   时间复杂度O(1)
+    # 队尾添加元素
     def push(self, item):
-        # 判断队列是否已满
-        if self.length() == self.size:
-            raise ValueError('队列已满')
         node = Node(item)
         # 如果队列为空
         if self.isEmpty():
@@ -45,8 +35,7 @@ class Queue:
             self.rear.next = node
             self.rear = node
         self._length += 1
-
-    # 抛出队首  时间复杂度O(1)
+    # 抛出队首
     def pop(self):
         if self.isEmpty():
             raise ValueError('队列是空的')
@@ -54,14 +43,11 @@ class Queue:
         self.head = self.head.next
         self._length -= 1
         return value
-
-    # 获取队首的值  时间复杂度O(1)
+    # 获取队首的值
     def peek(self):
         if self.isEmpty():
             raise ValueError('列表为空')
         return self.head.data
-
-
 if __name__ == '__main__':
     queue = Queue()
     queue.push(1)
@@ -77,3 +63,4 @@ if __name__ == '__main__':
     print(queue.pop())
     print(queue.pop())
     print(queue.pop())
+
